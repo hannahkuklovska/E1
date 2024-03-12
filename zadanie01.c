6f8701461138b0008dc5fc7c0e32c5b374bfe992
hannahkuklovska <hannah15072002@gmail.com>
2024-03-12 12:29:27 +0100

    podmienka, pre nultú odmocninu, respektíve jej nesprávnosť

--- zadanie01.c
+++ zadanie01.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+#define tol 0.0001 // presnost na 4 desatinne miesta
+
 int main(void)
 {
      double x;
      int m;
 
      return 0;
+}
+
+double mta_odmocnina(int m, double x) // mta odmocnina z x
+{
+     if (m == 0)
+     {
+          printf("Chyba: nultá odmocnina z čísla.");
+          return -1;
+     }
 }
